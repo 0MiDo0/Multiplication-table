34b8f4047007fa9ff8ccdd820c2a1c76ff96beba
Hao Feng <64658849+0MiDo0@users.noreply.github.com>
2020-05-02 16:14:47 +0200

    Add files via upload

---
+++ Multiplication table.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//int i = 1;
+	//int j = 1;
+	for (int i = 1; i < 10; i++)
+	{
+		for (int j=1;j<=i;j++)
+		{
+			cout << i << " * " << j << " = " << i * j << "  ";
+		}
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+//i 1 1 1 2 2

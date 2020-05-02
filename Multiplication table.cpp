a9dfd83b6f12e92c96b1b5b2194c93c3afcbb0df
Hao Feng <64658849+0MiDo0@users.noreply.github.com>
2020-05-02 16:15:22 +0200

    Update Multiplication table.cpp

--- Multiplication table.cpp
+++ Multiplication table.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 int main()
 {
-	//int i = 1;
-	//int j = 1;
 	for (int i = 1; i < 10; i++)
 	{
 		for (int j=1;j<=i;j++)
@@ -17,4 +15,3 @@ int main()
 	system("pause");
 	return 0;
 }
-//i 1 1 1 2 2
